d81dc312f77a9cc0baaf19ee93599c240329b757
JoanPablo <johnpjm85@gmail.com>
2021-11-25 14:19:24 +0200

    create spring boot project

---
+++ src/test/java/com/rideco/grocery/GroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideco.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
