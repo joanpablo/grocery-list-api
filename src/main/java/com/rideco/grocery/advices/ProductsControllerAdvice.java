c7fb5511b8aa73ee3e1a02057205eece0bf3e14b
JoanPablo <johnpjm85@gmail.com>
2021-11-26 14:46:44 +0200

    add generic exception handler

--- src/main/java/com/rideco/grocery/advices/ProductsControllerAdvice.java
+++ src/main/java/com/rideco/grocery/advices/ProductsControllerAdvice.java
@@ -3,10 +3,13 @@ package com.rideco.grocery.advices;
 import com.rideco.grocery.exceptions.ProductNotFoundException;
 import com.rideco.grocery.models.ErrorResponse;
 import com.rideco.grocery.models.ErrorResponseCode;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ProductsControllerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
